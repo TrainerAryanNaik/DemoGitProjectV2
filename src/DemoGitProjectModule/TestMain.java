2993588d2c53e0a2949bc073b15b2eb420f1a225
USTUser <USTUser@F280DTRV.ustr.com>
2022-03-09 19:34:51 +0530

    this is to commit my program

---
+++ src/DemoGitProjectModule/TestMain.java
@@ -0,0 +1,15 @@
+package DemoGitProjectModule;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		int a=20;
+		if(a==20) {
+			System.out.println("a is an even number");
+		}
+		else {
+			System.out.println("not an even number");
+		}
+	}
+
+}
