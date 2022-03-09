35d4df80dd0b3437005130ea8429676a56af473b
Jaya <JERIN@192.168.1.35>
2022-03-09 17:46:01 +0530

    commit to test3

---
+++ src/DemoGitProjectModule/Test3.java
@@ -0,0 +1,12 @@
+package DemoGitProjectModule;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		int a=10,b=5;
+		int p;
+		p=a*b;
+		System.out.println("product=" +p);
+	}
+
+}
