69b61609e0e7de0d5a62687ba5fadf827d5fb5a5
ritis <ritis@RITISH.mshome.net>
2022-03-09 19:58:27 +0530

    Commiting Demo to Branch_ritish

--- src/DemoGitProjectModule/Test3.java
+++ src/DemoGitProjectModule/Test3.java
@@ -2,12 +2,12 @@ package DemoGitProjectModule;
 
 public class Test3 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		int a=10,b=20;
 		int sum;
-		sum = a-b;
-		System.out.println("sum: "+sum);
+		sum=a-b;
+		System.out.println("sum :"+sum);
 	}
 
 }
