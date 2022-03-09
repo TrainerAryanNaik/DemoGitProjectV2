2ebbd05687564f95a173c4ac6ec37cf0794bce7c
Elijah joseph <Elijah joseph@LAPTOP-ASV9JAGQ>
2022-03-09 17:28:24 +0530

    Commited to test3.java

---
+++ src/DemoGitProjectModule/Test3.java
@@ -0,0 +1,14 @@
+package DemoGitProjectModule;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		int a = 10 , sum = 0;
+		while(a>0) {
+			sum = sum+a;
+			a--;
+		}
+		System.out.println("Sum = " + sum);
+	}
+
+}
