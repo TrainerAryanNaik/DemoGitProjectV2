3a13eb5dc88958a9a46b7e46752d6cda8e9ee224
ritis <ritis@RITISH.mshome.net>
2022-03-09 19:32:22 +0530

    Git Sum code

---
+++ src/DemoGitProjectModule/Test3.java
@@ -0,0 +1,13 @@
+package DemoGitProjectModule;
+
+public class Test3 {
+
+	public static void main(String[] args) 
+	{
+		int a=10,b=20;
+		int sum;
+		sum = a+b;
+		System.out.println("sum: "+sum);
+	}
+
+}
