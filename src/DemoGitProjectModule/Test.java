bcd25029cf2271f86ef179b715254648e4ee3df3
D keerthi <Dell@DESKTOP-KCSLH99>
2022-03-10 10:51:35 +0530

    commit test

---
+++ src/DemoGitProjectModule/Test.java
@@ -0,0 +1,19 @@
+package DemoGitProjectModule;
+
+public class Test {
+
+	public static void main(String[] args) {
+     int a=15,sum=0;
+     
+    	 while(a>0)
+     
+     {
+    	 sum=sum+a;
+    	 {
+    		 a--;
+    	 }
+     }
+     System.out.println("sum="+sum);
+	}
+
+}
