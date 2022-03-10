3d06fcb333c0d12721661654cb0bc66f2320ffbc
DELL <DELL@DESKTOP-67AAABU>
2022-03-10 11:17:48 +0530

    # WARNING: head commit changed in the meantime
    commit  to test5.java

---
+++ src/DemoGitProjectModule/test5.java
@@ -0,0 +1,15 @@
+package DemoGitProjectModule;
+
+public class test5 {
+
+	public static void main(String[] args) {
+			int age = 20;
+			if (age >= 18)
+				System.out.println("Donate Blood");
+			else {
+				System.out.println("Not eligible to donate blood");
+			}
+
+	}
+
+}
