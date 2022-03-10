65536c03e51118b184f07536ab23364dc547f24e
Castro K Joseph <castrojoseph10@gmail.com>
2022-03-10 12:56:11 +0530

    Sum code

--- src/DemoGitProjectModule/Test2.java
+++ src/DemoGitProjectModule/Test2.java
@@ -3,7 +3,7 @@ package DemoGitProjectModule;
 public class Test2 {
 
 	public static void main(String[] args) {
-		int a = 10 , sum = 0;
+		int a = 15 , sum = 0;
 		while(a>0) {
 			sum = sum+a;
 			a--;
