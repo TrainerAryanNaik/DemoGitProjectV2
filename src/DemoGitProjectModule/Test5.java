6be43e7566a7bd312c730b19b1ee702e8ca4decb
NEYMAR <NEYMAR@LAPTOP-MUHQ9C45>
2022-03-10 12:13:22 +0530

    first commit

---
+++ src/DemoGitProjectModule/Test5.java
@@ -0,0 +1,15 @@
+package DemoGitProjectModule;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+       int a=5,sum=0;
+       while(a>0)
+       {
+    	   sum=sum+a;
+    	   a--;
+       }
+       System.out.println("sum" +sum);
+	}
+
+}
